1eece5f22801a324f8a472e672d4dda3488953f9
ChrisLlanos <chrisllanos94@gmail.com>
2023-09-21 19:39:46 -0500

    added seeded questions and changed file names

--- seeds/quizData.js
+++ seeds/quizData.js
@@ -1,10 +1,30 @@
 
 const quizData = [
     {
-      question: ""
-      choices: ['1', '2', '3', '4'],
+      question: 'Tengo hambre quiero comer',
+      choices: ['I am tired I need to rest', 'I am hungry I want to eat', 'I am scared I want to hide', 'I am happy I want to dance'],
+      correctAnswer: 1, // array index of the right answer
+    },
+    
+    {
+      question: 'Estoy cansado necesito descansar',
+      choices: ['I am tired I need to rest', 'I am hungry I want to eat', 'I am scared I want to hide', 'I am happy I want to dance'],
       correctAnswer: 0, // array index of the right answer
     },
+    
+    {
+      question: 'Estoy contento quiero bailar',
+      choices: ['I am tired I need to rest', 'I am hungry I want to eat', 'I am scared I want to hide', 'I am happy I want to dance'],
+      correctAnswer: 3, // array index of the right answer
+    },
+
+    {
+      question: 'Estoy asustado quiero esconderme',
+      choices: ['I am tired I need to rest', 'I am hungry I want to eat', 'I am scared I want to hide', 'I am happy I want to dance'],
+      correctAnswer: 2, // array index of the right answer
+    }
+
+    
   ];
   
   module.exports = quizData;
