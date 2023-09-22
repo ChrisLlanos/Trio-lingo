150d5d3bcbd14e4d019042e276f30bb6b1f16ba3
Fancy Nancy <ramireznancy.686@gmail.com>
2023-09-21 19:48:16 -0600

    added get route for quiz

--- controllers/api/quizRoutes.js
+++ controllers/api/quizRoutes.js
@@ -1,34 +1,34 @@
-const router = require('express').Router();
-const { Project } = require('../../models');
+const router = require("express").Router();
+const { Quiz, Question } = require("../../models");
 
-router.post('/', async (req, res) => {
+router.get("/:quizId", async (req, res) => {
   try {
-    const newProject = await Project.create({
-      ...req.body,
-      user_id: req.session.user_id,
+    const quizId = req.params.quizId;
+
+    const quiz = await Quiz.findByPk(quizId, {
+      include: [
+        {
+          model: Question,
+          attributes: [
+            "question",
+            "choices",
+            "correctAnswer",
+            "language",
+            "createdAt",
+            "updatedAt",
+          ],
+        },
+      ],
     });
 
-    res.status(200).json(newProject);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-router.delete('/:id', async (req, res) => {
-  try {
-    const projectData = await Project.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
-
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    if (!quiz) {
+      res.status(404).json({ message: "Quiz not found" });
       return;
     }
 
-    res.status(200).json(projectData);
+    const quizQuestions = quiz.Questions || [];
+
+    res.status(200).json({ quiz, quizQuestions });
   } catch (err) {
     res.status(500).json(err);
   }
--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const { Model, DataTypes } = require("sequelize");
 const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
+const Quiz = require("./quiz");
 
 class User extends Model {
   checkPassword(loginPw) {
