f329d46e68c1d6f52f3a232876346b0e23f424ac
ChrisLlanos <chrisllanos94@gmail.com>
2023-09-25 20:40:50 -0500

    updated languageRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const quizRoutes = require("./quizRoutes");
+const languageRoutes = require("./languageRoutes");
 
 router.use("/users", userRoutes);
 router.use("/progress", quizRoutes);
+router.use("/language", languageRoutes);
 
 module.exports = router;
