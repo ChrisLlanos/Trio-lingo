333a181b14746a9fbc501145ea43db5d970678b9
Fancy Nancy <ramireznancy.686@gmail.com>
2023-09-26 10:17:20 -0600

    json file returned with quiz data in quiz routes

--- controllers/api/languageRoutes.js
+++ controllers/api/languageRoutes.js
@@ -1,21 +1,19 @@
 const router = require("express").Router();
-const {Quiz} = require("../../models/quiz");
-
-
+const { Quiz } = require("../../models/quiz");
 
 router.get("/", async (req, res) => {
-    res.render("languageSelection");
-    
-    // try {
-    //   const languages = await Quiz.findAll({
-    //     attributes: ["language"],
-    //     group: ["language"],
-    //   });
-    //   const languageOptions = languages.map((quiz) => quiz.language);
-    //   res.json({ languages: languageOptions });
-    // } catch (err) {
-    //   res.status(500).json(err);
-    // }
-  });
+  res.render("languageSelection");
 
-  module.exports = router;
+  // try {
+  //   const languages = await Quiz.findAll({
+  //     attributes: ["language"],
+  //     group: ["language"],
+  //   });
+  //   const languageOptions = languages.map((quiz) => quiz.language);
+  //   res.json({ languages: languageOptions });
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
+});
+
+module.exports = router;
--- controllers/api/quizRoutes.js
+++ controllers/api/quizRoutes.js
@@ -1,37 +1,10 @@
 const router = require("express").Router();
 const { Quiz, Question } = require("../../models");
 
-router.get("/:quizId", async (req, res) => {
-  try {
-    const quizId = req.params.quizId;
-
-    const quiz = await Quiz.findByPk(quizId, {
-      include: [
-        {
-          model: Question,
-          attributes: [
-            "question",
-            "choices",
-            "correctAnswer",
-            "language",
-            "createdAt",
-            "updatedAt",
-          ],
-        },
-      ],
-    });
-
-    if (!quiz) {
-      res.status(404).json({ message: "Quiz not found" });
-      return;
-    }
-
-    const quizQuestions = quiz.Questions || [];
-
-    res.status(200).json({ quiz, quizQuestions });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.get("/", async (req, res) => {
+  Quiz.findAll().then((quizData) => {
+    res.json(quizData);
+  });
 });
 
 module.exports = router;
