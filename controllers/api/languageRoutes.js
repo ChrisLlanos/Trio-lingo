7e1546c23d40f1b643ba15858e30d41b6b93e0b3
Fancy Nancy <ramireznancy.686@gmail.com>
2023-09-26 18:56:05 -0600

    added signup form to login handlebars

--- controllers/api/languageRoutes.js
+++ controllers/api/languageRoutes.js
@@ -3,17 +3,6 @@ const { Quiz } = require("../../models/quiz");
 
 router.get("/", async (req, res) => {
   res.render("languageSelection");
-
-  // try {
-  //   const languages = await Quiz.findAll({
-  //     attributes: ["language"],
-  //     group: ["language"],
-  //   });
-  //   const languageOptions = languages.map((quiz) => quiz.language);
-  //   res.json({ languages: languageOptions });
-  // } catch (err) {
-  //   res.status(500).json(err);
-  // }
 });
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,4 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const { User } = require("../models/user");
 const { Progress } = require("../models/progress");
 const { Quiz } = require("../models/quiz");
@@ -8,17 +8,6 @@ const progressData = require("../seeds/progressData");
 router.get("/", async (req, res) => {
   try {
     res.render("home");
-    // const quizzes = await Quiz.findAll();
-    // const userProgress = await Progress.findAll({
-    //   where: { user_id: req.session.user_id },
-    // });
-
-    // console.log(req.session.logged_in);
-    // res.render("home", {
-    //   loggedIn: loggedIn,
-    //   quizData: quizzes,
-    //   progressData: userProgress,
-    // });
   } catch (error) {
     console.error(error);
     res.status(500).send("Server Error");
