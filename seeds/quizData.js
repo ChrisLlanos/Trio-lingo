60a1aa99b95e8d6ebae7d4a1cbfa8b5e7dd90c82
Kanabaki <Kagomekanabaki@gmail.com>
2023-09-26 19:41:13 -0500

    added jp questions

--- seeds/quizData.js
+++ seeds/quizData.js
@@ -46,6 +46,44 @@ const quizData = [
     correctAnswer: 2, // array index of the right answer
     language: "Spanish",
   },
+  // Japanese Section =============================================
+  {
+    question: "What writing systems does Japanese contain?",
+    choices: [
+      "Hiragana and katakana",
+      "hiragana katakana and kanji",
+      "only kanji", 
+      "kanji and hiragana",
+    ],
+    correctAnswer: 1, // array index of the right answer
+    language: "Japanese",
+  },
+
+  {
+    question: "How would you say “Hello” in Japanese?",
+    choices: [
+      "こんにちは konnichiwa",
+      "さようなら sayounara",
+      "おはよう ohayou",
+      "おやすみなさい oyasuminasai",
+    ],
+    correctAnswer: 0, // array index of the right answer
+    language: "Japanese",
+  },
+
+  {
+    question: "What is bread in Japanese?",
+    choices: [
+      "みどり midori",
+      "スプーン supuun",
+      "パン pan",
+      "はし hashi"
+
+    ],
+    correctAnswer: 2, // array index of the right answer
+    language: "Japanese",
+  },
+
 ];
 
 module.exports = quizData;
